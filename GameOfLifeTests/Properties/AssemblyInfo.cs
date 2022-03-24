1867ea456194cf092781cfa04f697cf9d50966a2
Kruti Mohanta <krmohant@microsoft.com>
2022-03-24 11:03:36 +0530

    Add project files.

---
+++ GameOfLifeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GameOfLifeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft IT")]
+[assembly: AssemblyProduct("GameOfLifeTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft IT 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84f62a68-3fae-469e-8b95-2945e6952308")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
